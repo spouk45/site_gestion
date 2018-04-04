8d15e1dbf1bfb6fc6890b979b67344f37d7d2f4c
Yoan Beauchamp <34508060+spouk45@users.noreply.github.com>
2018-04-04 17:35:01 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
 
 try{
-	$db=new PDO('mysql:host='.$host.';dbname='.$dbname,'root',$password);
+	$db=new PDO('mysql:host='.$host.';dbname='.$dbname,$username,$password);
 	$db->exec("SET CHARACTER SET utf8");
 }
 catch (Exception $e){	echo '<p> Erreur SQL:'.$e->getMessage().'<br>';	exit();}
